f8142c777aec069530e8c7c18e7014dad6c25755
m.numata <nasakinumata@gmail.com>
2025-12-12 14:13:34 +0900

    Reapply "平均をとて表示する用に変更"
    This reverts commit afaf4f43e88ea4427e46e7c872ad655bb13d77ee.

--- src/src/task_measure.cpp
+++ src/src/task_measure.cpp
@@ -3,6 +3,11 @@
 #include <task.h>
 #include "shared.h"
 
+const int NUM_SAMPLES = 10;
+
+float rpmHistory[NUM_SAMPLES] = {0};
+int historyIndex = 0;
+
 void onSensorPulse() {
     unsigned long now = micros();
     unsigned long diff = now - lastPulseTime;
@@ -27,13 +32,29 @@ void TaskMeasure(void *pvParameters){
             taskEXIT_CRITICAL();
 
             if (interval >0) {
-                currentRPM = 60000000.0 / interval;
+
+                float instantRPM = 60000000.0 / interval;
+
+                rpmHistory[historyIndex] = instantRPM;
+                historyIndex = (historyIndex + 1) % NUM_SAMPLES;
+
+                float sum = 0;
+                for (int i = 0; i < NUM_SAMPLES; i++) {
+                    sum += rpmHistory[i];
+
+                }
+
+                currentRPM = sum / NUM_SAMPLES;
             }
 
         }
         
         if (micros() - lastPulseTime > 1000000) {
+
             currentRPM = 0;
+            for (int i = 0; i < NUM_SAMPLES; i++) {
+                rpmHistory[i] = 0.0;
+            }
         }
         
         vTaskDelay(pdMS_TO_TICKS(20));
